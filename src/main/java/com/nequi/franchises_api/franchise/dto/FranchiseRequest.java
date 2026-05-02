2372b9786cad993a40397b4582887665d64530dc
Juan Rodas <juan.rodas.manez@gmail.com>
2026-05-02 15:24:08 -0500

    refactor: modernize franchise DTOs and remove Lombok

--- src/main/java/com/nequi/franchises_api/franchise/dto/FranchiseRequest.java
+++ src/main/java/com/nequi/franchises_api/franchise/dto/FranchiseRequest.java
@@ -1,19 +1,20 @@
 package com.nequi.franchises_api.franchise.dto;
 
 import jakarta.validation.constraints.NotBlank;
-import lombok.Data;
 
-@Data
 public class FranchiseRequest {
 
     @NotBlank(message = "Name is required")
     private String name;
 
-    public FranchiseRequest(String name) {
-        this.name = name;
+    public FranchiseRequest() {
     }
 
     public String getName() {
         return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 }
--- src/main/java/com/nequi/franchises_api/franchise/dto/FranchiseResponse.java
+++ src/main/java/com/nequi/franchises_api/franchise/dto/FranchiseResponse.java
@@ -1,22 +1,7 @@
 package com.nequi.franchises_api.franchise.dto;
 
-import lombok.Data;
-
-@Data
-public class FranchiseResponse {
-    private Long id;
-    private String name;
-    
-    public FranchiseResponse(Long id, String name) {
-        this.id = id;
-        this.name = name;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
+public record FranchiseResponse(
+    Long id,
+    String name
+) {
 }
